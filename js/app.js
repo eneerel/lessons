a94d0ec9b4fb2689c9751cd22a31073474021f8b
Evening <evening@mstars-lab3-07s-iMac.local>
2022-11-28 17:36:45 +0800

    js

---
+++ js/app.js
@@ -0,0 +1,50 @@
+console.log("hello World")
+
+let ner = ( "enerel")
+let age = ( 20 )
+ 
+console.log(ner);
+console.log(age);
+
+console.log (typeof name );
+console.log(typeof age);
+
+
+// Comments can make code readable
+// javascript lesson begin
+
+let massege = true;
+console.log(massege);
+console.log(typeof massege);
+
+let ms = "";
+console.log(ms);
+console.log(typeof ms);
+
+let ovog ;
+console.log(ovog);
+console.log(typeof ovog);
+
+let nas = "null" ;
+console.log(nas);
+console.log(typeof nas);
+
+const P_TOO = 3.14;
+
+
+
+let gurwaljingiin_x = 2;
+let gurwaljingiin_y = 3;
+let gurwaljingiin_h = 5;
+console.log(gurwaljingiin_x+gurwaljingiin_y+gurwaljingiin_h )
+
+let x1 =1;
+let x2 =2;
+let y1 =3;
+let y2 =4;
+console.log( y2-y1 / x2-x1)
+
+let gegabyte = 15;
+let bit = 15*1024*1024*1024+8;
+console.log(15*1024*1024*1024+8)
+
